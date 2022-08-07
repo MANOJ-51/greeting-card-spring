fac978e35f8c3a5a98f830b1ab85b53f311dae68
MANOJ-51 <m4anoj51@gmail.com>
2022-08-07 22:11:35 +0530

    greeting app setup

---
+++ test/java/com/bridgelabz/greetingcardspring/GreetingCardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingCardSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
